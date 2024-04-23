88153898bd684ae1b7eb53bb685287db0205f122
jereilfeb2004 <jereilfeb2004@gmail.com>
2024-04-23 19:19:11 +0800

    provider.tf modification

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-southeast-1"  # Specify your desired AWS region
-  access_key = "AKIA2UC3B3ROYWP26X6D"
-  secret_key = "BRKnbY12YtOZY6Qb/p4CgfqjoVE5CNYzznh8fHXo"
+  access_key = "AWS_Key"
+  secret_key = "aws-secrey"
 }
